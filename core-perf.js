2b91cb2a0a66fa1da986f076262837362abfff14
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-03-10 21:16:18 +0000

    Parsing data

--- core-perf.js
+++ core-perf.js
@@ -3,6 +3,7 @@ var api = {
         var data = window.performance.getEntriesByType('mark');
 
         this.createTimeline();
+        this.parseData(data);
     },
 
     createTimeline: function() {
@@ -14,11 +15,23 @@ var api = {
             position: 'fixed',
             top: 0,
             bottom: 0,
-            width: 100%,
-            height: 3em,
+            width: '100%',
+            height: '3em',
             backgroundColor: 'black'
         });
         body.append(timeline);
+    },
+
+    parseData: function(data) {
+        console.log(_.chain(data)
+        .map(function(item) {
+            return {
+                name: item.name,
+                time: item.startTime
+            }
+        })
+        .sortBy('time')
+        .value());
     }
 };
 
