a615a0422a23d6b388bd487e55d262f7d34a71a1
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-03-10 17:18:03 +0000

    Added timeline

--- core-perf.js
+++ core-perf.js
@@ -1,7 +1,15 @@
 var api = {
     renderData: function() {
         var data = window.performance.getEntriesByType('mark');
-        console.log(data);
+
+        this.createTimeline();
+    },
+
+    createTimeline: function() {
+        var timeline = document.createElement('div');
+        timeline.id = "timeline";
+        timeline.style.cssText = "position: fixed; z-index: 6000; left: 0, bottom: 0; background-color: black; width: 100%; height: 2em;";
+        document.body.appendChild(timeline);
     }
 };
 
