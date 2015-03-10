27ecfb590e539b16f39c27e19781dd34d45ad215
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-03-10 17:31:46 +0000

    Added libs

--- core-perf.js
+++ core-perf.js
@@ -8,7 +8,7 @@ var api = {
     createTimeline: function() {
         var timeline = document.createElement('div');
         timeline.id = "timeline";
-        timeline.style.cssText = "position: fixed; z-index: 6000; left: 0; bottom: -2em; background-color: black; width: 100%; height: 2em;";
+        timeline.style.cssText = "position: fixed; z-index: 6000; left: 0; bottom: 0; background-color: black; width: 100%; height: 2em;";
         document.body.appendChild(timeline);
     }
 };
