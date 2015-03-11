10af8d478f0f3f0e4a9c149a5084f6d42f4fc868
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-03-11 10:40:25 +0000

    Added vis lib

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
     chrome.tabs.executeScript(null, {
-        code: "javascript:(function(){var lodash=document.createElement('script');lodash.src='https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.5.0/lodash.min.js';document.body.appendChild(lodash);var jquery=document.createElement('script');jquery.src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js';document.body.appendChild(jquery);var el=document.createElement('script');el.src='https://uplne.github.io/pageperf/core-perf.js';document.body.appendChild(el);})();"
+        code: "javascript:(function(){var el=document.createElement('script');el.src='https://uplne.github.io/pageperf/core-perf.js';document.body.appendChild(el);})();"
     });
 });
--- core-perf.js
+++ core-perf.js
@@ -2,8 +2,29 @@ var api = {
     renderData: function() {
         var data = window.performance.getEntriesByType('mark');
 
+        this.loadScripts();
         this.createTimeline();
         this.parseData(data);
+    },
+
+    loadScripts: function() {
+        var lodash = document.createElement('script');
+        lodash.src = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.5.0/lodash.min.js';
+        document.body.appendChild(lodash);
+
+        var jquery = document.createElement('script');
+        jquery.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js';
+        document.body.appendChild(jquery);
+
+        var visjs = document.createElement('script');
+        visjs.src = 'https://cdnjs.cloudflare.com/ajax/libs/vis/3.10.0/vis.min.js';
+        document.body.appendChild(visjs);
+
+        var viscss = document.createElement('link');
+        viscss.setAttribute("rel", "stylesheet");
+        viscss.setAttribute("type", "text/css");
+        viscss.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/vis/3.10.0/vis.min.css");
+        document.body.appendChild(viscss);
     },
 
     createTimeline: function() {
@@ -32,6 +53,36 @@ var api = {
         })
         .sortBy('time')
         .value());
+    },
+
+    visualizeData: function(data) {
+        var container = document.createElement('div');
+        container.id = 'visualization';
+        document.body.append(container);
+
+        var arr = _.map(data, function(item, index) {
+            return {
+                id: index,
+                content: item.name,
+                start: item.time
+            }
+        })
+  
+        var items = new vis.DataSet(arr);
+
+        // Configuration for the Timeline
+        var options = {
+            editable: true,
+            start: arr[0].start,
+            end: arr[arr.length - 1].start
+        };
+
+        // Create a Timeline
+        var timeline = new vis.Timeline(
+            container,
+            items,
+            options
+        );
     }
 };
 
