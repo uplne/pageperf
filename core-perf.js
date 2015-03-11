6929467674d4b349da7350155520d02b890150c0
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-03-11 13:35:34 +0000

    Moved to google charts

--- core-perf.js
+++ core-perf.js
@@ -62,7 +62,7 @@ var api = {
             var chart = new google.visualization.Timeline(container);
             var dataTable = new google.visualization.DataTable();
 
-            dataTable.addColumn({ type: 'string', id: 'ID' });
+            dataTable.addColumn({ type: 'number', id: 'ID' });
             dataTable.addColumn({ type: 'string', id: 'Name' });
             dataTable.addColumn({ type: 'number', id: 'Start' });
 
